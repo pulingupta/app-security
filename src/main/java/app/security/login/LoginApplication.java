11543c172c543e1df40c733e6c39aeb3c5a1af73
Gupta <pulin.gupta@capgemini.com>
2020-02-20 07:08:28 +0200

    Initial commit for basic authentication functionality with spring security

---
+++ src/main/java/app/security/login/LoginApplication.java
@@ -0,0 +1,22 @@
+package app.security.login;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@RestController
+public class LoginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoginApplication.class, args);
+	}
+	
+	@GetMapping("/hello")
+	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+	return String.format("Hello %s!", name);
+	}
+
+}
